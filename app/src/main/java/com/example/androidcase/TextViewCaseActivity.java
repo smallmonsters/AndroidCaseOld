d890af582bfc30323c841e606299aa2a7786676e
smallmonsters <34727387+smallmonsters@users.noreply.github.com>
2023-02-23 17:03:59 +0800

    init

---
+++ app/src/main/java/com/example/androidcase/TextViewCaseActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidcase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TextViewCaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_view_case);
+    }
+}
